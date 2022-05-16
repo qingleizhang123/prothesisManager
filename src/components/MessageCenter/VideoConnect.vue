a5a4bf10ca73d9ef85233bb0cf24e1ecd79af4e0
zhangqinglei <zhangqinglei@zhangqingleideMacBook-Pro.local>
2022-05-16 23:20:22 +0800

    多人视频问题调试

--- src/components/MessageCenter/VideoConnect.vue
+++ src/components/MessageCenter/VideoConnect.vue
@@ -86,14 +86,14 @@ const createConnect = () => {
         socket.send(`join|${recieveid}|成功加入视频房间${props.userid}`);
       }
       if (value[0] == 'answer') {
-        const index = peers.findIndex(row => row.id === value[1]);
+        const index = peers.findIndex(row => row.usid === value[1]);
         answer = new RTCSessionDescription({
           type: 'answer',
           sdp: value[2]
         })
         if (index >= 0) {
-          peers[index].setLocalDescription(offer)
-          peers[index].setRemoteDescription(answer)
+          peers[index].peer.setLocalDescription(offer)
+          peers[index].peer.setRemoteDescription(answer)
         }
       }
       if (value[0] == 'candid') {
@@ -240,88 +240,6 @@ const onMessageSend = () => {
   socket.send(`message|${recieveid}|${value.value}`); // 发送信息
   value.value = ''; // 发送后清空输入框
 }
-
-/* message.log('信令通道（WebSocket）创建中......');
-
-socket.onopen = () => {
-  message.log('信令通道创建成功！');
-  target === 'offer' && (button.style.display = 'block');
-}
-socket.onerror = () => message.error('信令通道创建失败！');
-socket.onmessage = e => {
-  const { type, sdp, iceCandidate } = JSON.parse(e.data)
-  if (type === 'answer') {
-    peer.setRemoteDescription(new RTCSessionDescription({ type, sdp }));
-  } else if (type === 'answer_ice') {
-    peer.addIceCandidate(iceCandidate);
-  } else if (type === 'offer') {
-    startLive(new RTCSessionDescription({ type, sdp }));
-  } else if (type === 'offer_ice') {
-    peer.addIceCandidate(iceCandidate);
-  }
-};
-
-const PeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-!PeerConnection && message.error('浏览器不支持WebRTC！');
-const peer = new PeerConnection();
-
-peer.ontrack = e => {
-  if (e && e.streams) {
-    message.log('收到对方音频/视频流数据...');
-    remoteVideo.srcObject = e.streams[0];
-  }
-};
-
-peer.onicecandidate = e => {
-  if (e.candidate) {
-    message.log('搜集并发送候选人');
-    socket.send(JSON.stringify({
-      type: `${target}_ice`,
-      iceCandidate: e.candidate
-    }));
-  } else {
-    message.log('候选人收集完成！');
-  }
-};
-
-const startLive = async (offerSdp) => {
-  target === 'offer' && (button.style.display = 'none');
-  let stream;
-  try {
-    message.log('尝试调取本地摄像头/麦克风');
-    stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-    message.log('摄像头/麦克风获取成功！');
-    localVideo.srcObject = stream;
-  } catch {
-    message.error('摄像头/麦克风获取失败！');
-    return;
-  }
-
-  message.log(`------ WebRTC ${target === 'offer' ? '发起方' : '接收方'}流程开始 ------`);
-  message.log('将媒体轨道添加到轨道集');
-  stream.getTracks().forEach(track => {
-    peer.addTrack(track, stream);
-  });
-
-  if (!offerSdp) {
-    message.log('创建本地SDP');
-    const offer = await peer.createOffer();
-    await peer.setLocalDescription(offer);
-
-    message.log(`传输发起方本地SDP`);
-    socket.send(JSON.stringify(offer));
-  } else {
-    message.log('接收到发送方SDP');
-    await peer.setRemoteDescription(offerSdp);
-
-    message.log('创建接收方（应答）SDP');
-    const answer = await peer.createAnswer();
-    message.log(`传输接收方（应答）SDP`);
-    socket.send(JSON.stringify(answer));
-    await peer.setLocalDescription(answer);
-  }
-} */
-
 </script>
 
 <style lang="less" scoped>
